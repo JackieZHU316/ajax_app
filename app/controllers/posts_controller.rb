885445c8892d95bcd10f6f4d97ae2b4d30145790
JackieZHU316 <zhumingjie316@gmail.com>
2022-08-11 15:50:20 +0800

    メモ投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,19 @@ class PostsController < ApplicationController
 
   def index
     # @posts = Post.all
+    # @posts = Post.order(id: "DESC")
     @posts = Post.order(id: "DESC")
   end
 
+  # コメントアウトする
   # def new
   # end
 
   def create
     # binding.pry
-    post = Post.create(content: params[:content])
-    # redirect_to action: :index
-    render json:{ post: post }
+    # post = Post.create(content: params[:content])
+    Post.create(content: params[:content])
+    redirect_to action: :index
+    # render json:{ post: post }
   end
 end
